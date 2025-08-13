79ea3fb345d7c3ab0aba2cce18eeb0ab93dda1cb
dvalluru <dhruva.valluru@gmail.com>
2025-08-13 12:25:12 -0400

    Initial commit

---
+++ app/__generated__/$.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body, Link as Link } from "@webstudio-is/sdk-components-react-router";
+import { HtmlEmbed as HtmlEmbed } from "@webstudio-is/sdk-components-react";
+
+
+      export const projectId = "2361981d-cbcb-4af1-858a-c5279be0a9b4";
+
+      export const lastPublished = "2025-08-13T15:49:52.328Z";
+
+      export const siteName = undefined;
+
+      export const breakpoints = [{"id":"dk0jvRO1Q8FpsrY_3p7C2"},{"id":"vo6Zl280UcArVF1k9j2ZH","maxWidth":991},{"id":"yRv-EhAKz12s_QGkZF_ff","maxWidth":767},{"id":"bqN1ouIqk1MXfNEN_FPf3","maxWidth":479}];
+
+      export const favIconAsset: string | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: string[] =
+        []
+
+      export const pageBackgroundImageAssets: string[] =
+        []
+
+      
+
+      const Page = (_props: { system: any; }) => {
+return <Body
+className={`w-element c14htaox c1p5t8p7 c9kwla4 c64cshc`}>
+<div
+className={`w-element`}>
+<div
+className={`w-element cq20xiw c1rccg5r c1wi5bnt cxrpsnq c2s3cud cu3apsz`}>
+<div
+className={`w-element`}>
+{"404"}
+</div>
+<div
+className={`w-element c1ensgd9 c144hgfo ch98oe4 c1noer0e c1o6lg71 c1ehzbaq`}>
+{"404"}
+</div>
+<div
+className={`w-element c1ensgd9 c144hgfo c34up6a c1noer0e c1oa4670 c1ehzbaq`}>
+{"404"}
+</div>
+<div
+className={`w-element c1ensgd9 c13nzyjy c10711r c1kjb1n2 c64cshc cps7w8w`} />
+</div>
+<p
+className={`w-element c13fyy7a cc1gzh9 c9ji48i c17hx4a cdnsiob`}>
+{"PAGE NOT FOUND"}
+</p>
+</div>
+<Link
+href={"https://webstudio.is/?via=badge"}
+target={"_blank"}
+className={`w-element cwck2sl c1ovfj59 crporja c9kwla4 c1p5t8p7 c1jgztcf c171qtmt cuy5ujb cq9wa09 cpf1ixz ce61386 cfo8hj7 csismkq c1sl4ck7 c10d6dc7 curwjui co8cmfv c2s3cud c5i59ft cyx5sx1 ca36g4w czhszka c178u5re c4qe7x1 c1dj37hu c1f63703 c1txnr0b c1p3cp1g`}>
+<HtmlEmbed
+code={"<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" viewBox=\"0 0 16 16\" width=\"100%\" height=\"100%\" style=\"display: block;\"><path fill-rule=\"evenodd\" d=\"m12.32 12.416 2.62-8.085a1.205 1.205 0 1 0-2.292-.746l-2.62 8.084a1.205 1.205 0 1 0 2.292.747Z\" clip-rule=\"evenodd\"/><path fill-rule=\"evenodd\" d=\"M8 7.624c.297 0 .517.175.704.394.207.243.373.545.514.866.634 1.44.753 3.241.753 3.241a1.206 1.206 0 0 0 1.285 1.122 1.207 1.207 0 0 0 1.12-1.287s-.16-2.25-.952-4.05C10.744 6.364 9.594 5.208 8 5.208c-1.594 0-2.744 1.156-3.424 2.7-.792 1.8-.951 4.05-.951 4.05a1.207 1.207 0 0 0 1.12 1.288 1.206 1.206 0 0 0 1.284-1.122s.119-1.8.753-3.24a3.52 3.52 0 0 1 .514-.867c.187-.22.406-.394.704-.394Z\" clip-rule=\"evenodd\"/><path fill-rule=\"evenodd\" d=\"M5.973 11.669 3.352 3.585a1.205 1.205 0 1 0-2.293.746l2.622 8.084a1.205 1.205 0 1 0 2.292-.746Z\" clip-rule=\"evenodd\"/></svg>"}
+className={`w-html-embed c1p106cv cp15cxs cjenfxo c91mw2p`} />
+<div
+className={`w-element`}>
+{"Built with Webstudio"}
+</div>
+</Link>
+</Body>
+}
+
+
+      export { Page }
+    
---
+++ app/__generated__/_index.tsx
@@ -0,0 +1,371 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body, Link as Link, Link as Link_1 } from "@webstudio-is/sdk-components-react-router";
+import { Span as Span, Text as Text, Paragraph as Paragraph, Box as Box, Heading as Heading, Image as Image, ListItem as ListItem, List as List, HtmlEmbed as HtmlEmbed } from "@webstudio-is/sdk-components-react";
+
+
+      export const projectId = "2361981d-cbcb-4af1-858a-c5279be0a9b4";
+
+      export const lastPublished = "2025-08-13T15:49:52.328Z";
+
+      export const siteName = undefined;
+
+      export const breakpoints = [{"id":"dk0jvRO1Q8FpsrY_3p7C2"},{"id":"vo6Zl280UcArVF1k9j2ZH","maxWidth":991},{"id":"yRv-EhAKz12s_QGkZF_ff","maxWidth":767},{"id":"bqN1ouIqk1MXfNEN_FPf3","maxWidth":479}];
+
+      export const favIconAsset: string | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: string[] =
+        []
+
+      export const pageBackgroundImageAssets: string[] =
+        []
+
+      
+            
+
+            export const CustomCode = () => {
+              return (<></>);
+            }
+          
+
+      const Page = (_props: { system: any; }) => {
+return <Body
+className={`w-element c1ei3dmb`}>
+<Box
+data-ws-tag="div"
+className={`w-box c1e4xjch c1h2oasy cr90xn4 cvi8ya6 c64cshc cpcimay`}>
+<Box
+className={`w-box c1dj0mx4 c132qth c19rggdh ca2kioc c1pncp21 c94h7a3 c1augzz0 c1vnqez8 cq20xiw c1kjb1n2 c144hgfo c1qqs9zj c5mpygp c1p106cv c16ukf1j c1ks5wic c9qe7t creg84j c1u5pzqw czbas69 c15hjrq5 cgunpyb c1d8iaij cx81amu c1gqmc2b cm9e4u2 c1jfty5x cgtjuab c1vi1r5r c1j9h7ah cq32auv c4v0y2l cnlkdzc cjr4dg0 cbqwz93 co9x71u c1jmax7c c1qzv6t7 c3dfqs3 cth1q3b cz42wsi c1juvuue ccis1bc ctmx9a`}>
+<Box
+data-ws-tag="div"
+className={`w-box c1kjb1n2 ch4wvhc c1ks5wic c9qe7t cjjx8p6 c1ksqu68`}>
+<Box
+data-ws-tag="div"
+className={`w-box c14htaox chgf1zn c1ucpip5 cmlcxaa c6w8gpd c9kwla4 cyl55wp c1vz3gsd`}>
+<Link
+className={`w-link c1vyf5lu cce41c1 c1ogx30l c178u5re c1crwyo3 c18hqbk5 cq20xiw c1la3eta c1kjkc9u c1p2tpvh`}>
+<Image
+loading={"lazy"}
+width={"58"}
+height={"100"}
+src={"/assets/689a55891975c24556aacb99_favicon-3_lV3tAW0JRVCM6pfBUnzI6.png"}
+width={48}
+height={48}
+className={`w-image c4jinfl c1r5fsax czqmb3h cq20xiw cvozrr1 cmm9kbk crie2qp cceageo cccdx59 c1w5jq4t cgzwtr6 c7cijuw c15hjrq5 czxri0q ci1dfz5 c13gzh7w c11hjzmg c1fhflox`} />
+</Link>
+<Box
+data-ws-tag="nav"
+role={"navigation"}
+className={`w-box c1p106cv c1lt3wfw cq20xiw cyxxuue`}>
+<List
+className={`w-list cotgnhd c1qj633g cbjrvrf c14htaox c19j4fi c9kwla4 cq4v8dv c1kvb9tj c1eaduv1 cgey900 c54tkx4 c10pfrqo c85fl8e ct2qcpp ce1lopw cy3e3b9 ce5u5wr cmv4d5k c62sk5i c9n3sfv cvzgt0l cjpkc40 c1ax2dhe c1483luz c3cr3at`}>
+<ListItem
+className={`w-list-item c912rs7 cbmxi35 c1fe44tr c30llp9`}>
+<Link
+href={"#"}
+className={`w-link c1vyf5lu c1gw5epo c1r11phs c1o4xu46 cq9wa09 c1o86oj6 ce61386 c1mtc71i cqk3upc c13kxf24 c1suvhh8 cdbronx c178u5re c1crwyo3 c18hqbk5 c1dgdurv cc1gzh9 c1la3eta cysrw5f c1kjkc9u c6d4nq2 c1lw4101 cykmshi c1ffqh44 c5ryu5c cdwv7dr c193hbdq cmyqo4a c1paunlz c11alpps cz9jhwl ckphtoo`}>
+{"Home"}
+</Link>
+</ListItem>
+<ListItem
+className={`w-list-item c30llp9`}>
+<Link
+href={"#"}
+className={`w-link c1vyf5lu c1gw5epo c1r11phs c1o4xu46 cq9wa09 c1o86oj6 ce61386 c1mtc71i cqk3upc c13kxf24 c1suvhh8 cdbronx c178u5re c1crwyo3 c18hqbk5 cc1gzh9 c1la3eta cysrw5f c1kjkc9u c6d4nq2 c1lw4101 cykmshi c1ffqh44 c5ryu5c cdwv7dr c193hbdq cmyqo4a c1paunlz c11alpps cz9jhwl ckphtoo`}>
+{"Mission"}
+</Link>
+</ListItem>
+<ListItem
+className={`w-list-item c30llp9`}>
+<Link
+href={"#"}
+className={`w-link c1vyf5lu c1gw5epo c1r11phs c1o4xu46 cq9wa09 c1o86oj6 ce61386 c1mtc71i cqk3upc c13kxf24 c1suvhh8 cdbronx c178u5re c1crwyo3 c18hqbk5 cc1gzh9 c1la3eta cysrw5f c1kjkc9u c6d4nq2 c1lw4101 cykmshi c1ffqh44 c5ryu5c cdwv7dr c193hbdq cmyqo4a c1paunlz c11alpps cz9jhwl ckphtoo`}>
+{"Events"}
+</Link>
+</ListItem>
+<ListItem
+className={`w-list-item c30llp9`}>
+<Link
+href={"#"}
+className={`w-link c1vyf5lu c1gw5epo c1r11phs c1o4xu46 cq9wa09 c1o86oj6 ce61386 c1mtc71i cqk3upc c13kxf24 c1suvhh8 cdbronx c178u5re c1crwyo3 c18hqbk5 cc1gzh9 c1la3eta cysrw5f c1kjkc9u c6d4nq2 c1lw4101 cykmshi c1ffqh44 c5ryu5c cdwv7dr c193hbdq cmyqo4a c1paunlz c11alpps cz9jhwl ckphtoo`}>
+{"Companies "}
+</Link>
+<Link
+href={"#"}
+className={`w-link c1vyf5lu c1gw5epo c1r11phs c1o4xu46 cq9wa09 c1o86oj6 ce61386 c1mtc71i cqk3upc c13kxf24 c1suvhh8 cdbronx c178u5re c1crwyo3 c18hqbk5 cc1gzh9 c1la3eta cysrw5f c1kjkc9u c6d4nq2 c1lw4101 cykmshi c1ffqh44 c5ryu5c cdwv7dr c193hbdq cmyqo4a c1paunlz c11alpps cz9jhwl ckphtoo`}>
+{"Accelerator "}
+</Link>
+</ListItem>
+</List>
+</Box>
+<Box
+className={`w-box c1lt3wfw czs7dny c13u5sg0 c19didr4 cz9r9mi cep7sqa cq20xiw civqk9i c1n93unl cl1q1my cub164 c1awjvyp c6yyyrx cjzdlg1`}>
+<Box
+className={`w-box cg967zu`} />
+<Image
+loading={"lazy"}
+src={"/assets/placeholder.60f9b1840c_Iq4k3i6iNcwYnxkXxWxNf.svg"}
+width={140}
+height={140}
+className={`w-image ci35snv c1r5fsax czqmb3h c1mm8eqy`} />
+</Box>
+</Box>
+</Box>
+</Box>
+</Box>
+<Box
+data-ws-tag="div"
+className={`w-box c1mlj9i2 c64cshc`}>
+<Image
+loading={"lazy"}
+src={"/assets/689ad2d93edbd9e9a5f4351e_hero-image.fill.size_1248x702.v1735036519_eplZRX8EROCixOURwgQFU.jpg"}
+width={1248}
+height={702}
+className={`w-image ci35snv c1r5fsax czqmb3h cq20xiw cpv1psf c10eco64 c1nha97z c1gjb7aa c1mm8eqy`} />
+</Box>
+<Box
+data-ws-tag="section"
+className={`w-box c1p106cv cq20xiw cfa819k c15gq0q8 c2my1tl c5ty7yo c15hjrq5 c1o3hdwy c1qa921u c954hlh c1pw6dc6 c117bh2h cjm0v3w`}>
+<Box
+data-ws-tag="div"
+className={`w-box c1kjb1n2 ch4wvhc c1ks5wic c9qe7t cjjx8p6 c1ksqu68`}>
+<Box
+data-ws-tag="div"
+className={`w-box c14htaox c1r11phs c19j4fi cvzgt0l`}>
+<Box
+data-ws-tag="div"
+className={`w-box cnjfj5b cp8jvr cbecit c1hxrwrk c1l3k3t0 cxlslfl cpvsv2u`}>
+<Text
+data-ws-tag="div"
+className={`w-text-1 c365qd2 cyq38bn c22ioww cq20xiw ch04ciw c1glvn9a chgf1zn c15hjrq5 c1e2xgbf c1p4yw7t cygscrb c1fhki89 c1tsywmp c18x402v cs4t4yi cf0lnnb`}>
+{"BuildRTP"}
+</Text>
+<Heading
+data-ws-tag="h3"
+className={`w-heading c1bo3sak c1kjajrd c1l1csnu cz9r9mi ci8ocjg cq20xiw cdxiux8 c1f9s00m c1pootnl c1glvn9a c69n0ya cdmlw8q`}>
+{"For students, by students"}
+</Heading>
+<Link
+href={"#"}
+className={`w-link c70hi3u civqk9i c7j3u7t czs7dny cawahsu c1bv8jql c1g5v0ov cvu254e cce29sz c1o1f41i c1gca9f1 c178u5re c1crwyo3 c18hqbk5 czqmb3h cyicfru cq20xiw cgxfszs c17wngu7 c10931g2 c1huss1w c64ctqb c1w38h1d c1glvn9a c1icbuxb c69n0ya c1ux1j4n c22ioww c1rccg5r c1mm8eqy c1la3eta c1kjkc9u`}>
+{""}
+<br />
+{""}
+{" Learn More"}
+</Link>
+</Box>
+<Box
+data-ws-tag="div"
+className={`w-box c14htaox clfc3ah cmlcxaa cbvccl4 c164emw5 c1kcc5e3`}>
+<Box
+data-ws-tag="div"
+className={`w-box cq20xiw c10uoy1k c1rwhigx c1p106cv cytzbv5 chxss17 ccz1xyi c1hgccpb chgf1zn czbas69 c15hjrq5 c12ec3pd c11uw6o5 chw5jdp c15whf12 ccurhn4 cle38yp c1krrlq3 cd8cfmp c180enj5 c15qg4zb cuxu5cu c17v9nhw c1k7qpj2 c1kjajrd c9tr7sf`}>
+<Text
+data-ws-tag="div"
+className={`w-text-1 cq20xiw c1oajv4e c101lh96 c19tbvu2 c1e2xgbf cdmlw8q cxy1ymx cc1gzh9`}>
+{"$5,000+"}
+</Text>
+<Text
+data-ws-tag="div"
+className={`w-text-1 cq20xiw c1krbnui c1pl7bql cl1uwoh c1e2xgbf c1p4yw7t`}>
+{"funded to local teen startups"}
+</Text>
+</Box>
+<Box
+data-ws-tag="div"
+className={`w-box cq20xiw c10uoy1k c1rwhigx c1p106cv cytzbv5 chxss17 ccz1xyi c1hgccpb chgf1zn czbas69 c15hjrq5 c12ec3pd c11uw6o5 chw5jdp c15whf12 ccurhn4 cle38yp c1krrlq3 cd8cfmp c180enj5 c15qg4zb cuxu5cu cktnocx c1k7qpj2 c1kjajrd c9tr7sf`}>
+<Text
+data-ws-tag="div"
+className={`w-text-1 cq20xiw cdxiux8 c1609cgi cru2ilv cdmlw8q cxy1ymx cc1gzh9`}>
+{"16+ "}
+</Text>
+<Text
+data-ws-tag="div"
+className={`w-text-1 cq20xiw ch2dcpl c1vl4wx3 cxyqowq c1e2xgbf c1p4yw7t`}>
+{"supported local teen startups"}
+</Text>
+</Box>
+</Box>
+</Box>
+</Box>
+</Box>
+<Box
+data-ws-tag="section"
+className={`w-box c1p106cv cq20xiw csi75es c1la5gqk c8fokpy cppou0d cg27kfd c64cshc cnj2g6s c1qa921u c954hlh c1pw6dc6 c117bh2h cjm0v3w`}>
+<Box
+data-ws-tag="div"
+className={`w-box c1kjb1n2 ch4wvhc c1ks5wic c9qe7t cjjx8p6 c1ksqu68`}>
+<Box
+data-ws-tag="div"
+className={`w-box c14htaox c19j4fi cvzgt0l`}>
+<Box
+data-ws-tag="div"
+className={`w-box c14htaox clfc3ah c1p5t8p7 cbvccl4 crf2srf cbmxi35 cprfewa c10d6dc7 c1p4yw7t c7t5qy3`}>
+<Text
+data-ws-tag="div"
+className={`w-text-1 c365qd2 c1va3fdf c22ioww cq20xiw c16uqcgj c1bspuhp c1609cgi c6t65v3 c190ibhs c1uurkxc cdmlw8q c1rccg5r c1tsywmp c18x402v cs4t4yi cf0lnnb`}>
+{"Connect. Grow. "}
+<Span
+className={`w-text c1o3hdwy`}>
+{"Innovate."}
+</Span>
+</Text>
+<Text
+data-ws-tag="div"
+className={`w-text-1 cq20xiw c8oxoz2 c1j6lgxs c1uurkxc c1o3hdwy c1iskx1n c22ioww c1rccg5r`}>
+{""}
+<br />
+{""}
+{"WHY BuildRTP?"}
+</Text>
+<Paragraph
+className={`w-paragraph cotgnhd c14557kt cq20xiw cbcdiud c1nha97z ckbdjcc c1kjajrd c1rccg5r c1dgdurv`}>
+{""}
+<br />
+{""}
+{"BuildRTP is the first student-focused space in the "}
+<span
+className={`w-element c1o3hdwy`}>
+{"Research Triangle Park "}
+</span>
+{"area where any high schooler can come and develop their passions. "}
+{""}
+<br />
+{""}
+{"With an office space in RTP, we aim to provide a community to support driven high schoolers."}
+</Paragraph>
+</Box>
+</Box>
+</Box>
+<Box
+className={`w-box ch4wvhc c9qe7t c1ks5wic c1p106cv c1n74m5w c1e5txf5 c12c7bv8 cjjx8p6 c1gb9f4n cgtjuab c1vi1r5r c1j9h7ah cq32auv c4v0y2l cnlkdzc cjr4dg0 cbqwz93 co9x71u c1jmax7c c1qzv6t7 c3dfqs3 cth1q3b c1ksqu68`}>
+<Image
+loading={"lazy"}
+src={"/assets/689ae83843d1759e45ecaed4_Screenshot_202025-08-12_20at_203.03.58_E2_80_AFAM_YqQRvY6Us05VqHV_nrNzB.png"}
+width={847}
+height={805}
+className={`w-image ci35snv c1r5fsax czqmb3h c1f9s00m c1e5txf5 cd8cfmp c180enj5 c15qg4zb cuxu5cu c1l6guki c1mm8eqy`} />
+<Image
+loading={"lazy"}
+src={"/assets/689ae82e3a6461ac091d6f09_Screenshot_202025-08-12_20at_203.05.11_E2_80_AFAM_MpEDAzSbcezvUJ7SxonAq.png"}
+width={833}
+height={807}
+className={`w-image ci35snv c1r5fsax czqmb3h cq20xiw c1ypiku7 c1f9s00m c1e5txf5 cd8cfmp c180enj5 c15qg4zb cuxu5cu c1nhys62 c1mm8eqy`} />
+<Image
+loading={"lazy"}
+src={"/assets/689ae82eca4114bb4526f74e_Screenshot_202025-08-12_20at_203.01.36_E2_80_AFAM_lRZA5j-OUDEKR4-gx3oVV.png"}
+width={836}
+height={802}
+className={`w-image ci35snv c1r5fsax czqmb3h cq20xiw c1h8iqp9 c1f9s00m c1e5txf5 cd8cfmp c180enj5 c15qg4zb cuxu5cu c1w69tui c1mm8eqy`} />
+</Box>
+</Box>
+<Box
+data-ws-tag="section"
+className={`w-box c1p106cv cq20xiw c1la5gqk c8fokpy c5ty7yo cg27kfd c1qa921u c954hlh c1pw6dc6 c117bh2h cjm0v3w`}>
+<Box
+data-ws-tag="div"
+className={`w-box c1kjb1n2 ch4wvhc c1ks5wic c9qe7t cjjx8p6 c1ksqu68`}>
+<Heading
+data-ws-tag="h2"
+className={`w-heading cq3vjgb c58fszl c1a7wpak c13j9i2u cisp335 cq20xiw cbwkiz9 c1e2xgbf cdmlw8q c1rccg5r c12c95bl`}>
+{"Our Sponsers"}
+</Heading>
+<Paragraph
+className={`w-paragraph cotgnhd cd4xdp7 cq20xiw c13v1rt2 c1y72poh c1ks5wic c9qe7t c1uurkxc c15g6rdc c22ioww c1rccg5r`}>
+{"Join our community of sponsors and help students across the area "}
+<Span
+className={`w-text c1o3hdwy`}>
+{"achieve their goals."}
+</Span>
+</Paragraph>
+<Box
+data-ws-tag="div"
+className={`w-box cz7upkt c1q9eio0 chw5jdp ccg3v23 c1rxar17 c1pc5ltt c1bza10r clpu0xs c11k0x1w`}>
+<Box
+data-ws-tag="div"
+className={`w-box c4ds1fh c1thbgsy c11k6d3n cbuc4hm c14htaox clfc3ah cmlcxaa c9kwla4 c1rccg5r`}>
+<Image
+loading={"lazy"}
+src={"/assets/689adaaba224a58efd4b6eae_Screenshot_202025-08-12_20at_202.07.35_E2_80_AFAM_eV2TFM1-RV8bKrdMEt8nQ.png"}
+width={371}
+height={82}
+className={`w-image ci35snv c1r5fsax czqmb3h c1mm8eqy`} />
+<Heading
+data-ws-tag="h3"
+className={`w-heading cd4xdp7 c58fszl c1a7wpak cz9r9mi ci8ocjg c1x7wrcq`}>
+{""}
+<br />
+{""}
+{"ThinkSabio"}
+</Heading>
+<Paragraph
+className={`w-paragraph cotgnhd c12c0nox`}>
+{"Platinum Sponser"}
+</Paragraph>
+</Box>
+<Box
+data-ws-tag="div"
+className={`w-box c4ds1fh c1thbgsy c11k6d3n cbuc4hm c14htaox clfc3ah cmlcxaa c9kwla4 c1rccg5r`}>
+<Image
+loading={"lazy"}
+src={"/assets/689adaabbe61863aa1f98707_Screenshot_202025-08-12_20at_202.07.51_E2_80_AFAM_-TTPfudNiUKsrtk_aJ9eM.png"}
+width={442}
+height={415}
+className={`w-image ci35snv c1r5fsax czqmb3h cykkml8 c1dvwdff cq3vjgb cko8yqn c1mm8eqy`} />
+<Heading
+data-ws-tag="h3"
+className={`w-heading cd4xdp7 c58fszl c1a7wpak cz9r9mi ci8ocjg c13a1tcg`}>
+{"Rajan RealEstate"}
+</Heading>
+<Paragraph
+className={`w-paragraph cotgnhd c12c0nox`}>
+{"Gold Sponser"}
+</Paragraph>
+</Box>
+<Box
+data-ws-tag="div"
+className={`w-box c4ds1fh c1thbgsy c11k6d3n cbuc4hm c14htaox clfc3ah cmlcxaa c9kwla4 c1rccg5r`}>
+<Image
+loading={"lazy"}
+width={"152"}
+src={"/assets/689adaab2a3c78aa662c56ce_Screenshot_202025-08-12_20at_202.08.43_E2_80_AFAM_yWanSx2jO9p3QogXdmtXJ.png"}
+width={667}
+height={376}
+className={`w-image ci35snv c1r5fsax czqmb3h cq20xiw c1n3hst3 c1mm8eqy`} />
+<Heading
+data-ws-tag="h3"
+className={`w-heading cd4xdp7 c58fszl c1a7wpak cz9r9mi ci8ocjg cwqudjb cq20xiw c1n3hst3`}>
+{"Cactus Capital"}
+</Heading>
+<Paragraph
+className={`w-paragraph cotgnhd c12c0nox cq20xiw c1n3hst3`}>
+{"Bronze Sponser"}
+</Paragraph>
+</Box>
+</Box>
+</Box>
+<Link_1
+href={"https://webstudio.is/?via=badge"}
+target={"_blank"}
+className={`w-element cwck2sl c1ovfj59 crporja c9kwla4 c1p5t8p7 c1jgztcf c171qtmt cuy5ujb cq9wa09 cpf1ixz ce61386 cfo8hj7 csismkq c1sl4ck7 c10d6dc7 curwjui co8cmfv c2s3cud c5i59ft cyx5sx1 ca36g4w czhszka c178u5re c4qe7x1 c1dj37hu c1f63703 c1txnr0b c1p3cp1g`}>
+<HtmlEmbed
+code={"<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" viewBox=\"0 0 16 16\" width=\"100%\" height=\"100%\" style=\"display: block;\"><path fill-rule=\"evenodd\" d=\"m12.32 12.416 2.62-8.085a1.205 1.205 0 1 0-2.292-.746l-2.62 8.084a1.205 1.205 0 1 0 2.292.747Z\" clip-rule=\"evenodd\"/><path fill-rule=\"evenodd\" d=\"M8 7.624c.297 0 .517.175.704.394.207.243.373.545.514.866.634 1.44.753 3.241.753 3.241a1.206 1.206 0 0 0 1.285 1.122 1.207 1.207 0 0 0 1.12-1.287s-.16-2.25-.952-4.05C10.744 6.364 9.594 5.208 8 5.208c-1.594 0-2.744 1.156-3.424 2.7-.792 1.8-.951 4.05-.951 4.05a1.207 1.207 0 0 0 1.12 1.288 1.206 1.206 0 0 0 1.284-1.122s.119-1.8.753-3.24a3.52 3.52 0 0 1 .514-.867c.187-.22.406-.394.704-.394Z\" clip-rule=\"evenodd\"/><path fill-rule=\"evenodd\" d=\"M5.973 11.669 3.352 3.585a1.205 1.205 0 1 0-2.293.746l2.622 8.084a1.205 1.205 0 1 0 2.292-.746Z\" clip-rule=\"evenodd\"/></svg>"}
+className={`w-html-embed c1p106cv cp15cxs cjenfxo c91mw2p`} />
+<div
+className={`w-element`}>
+{"Built with Webstudio"}
+</div>
+</Link_1>
+</Box>
+</Body>
+}
+
+
+      export { Page }
+    
